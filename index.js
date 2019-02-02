d72f2026d527b6af1e1f34723d22f5ea3cd943bf
G. Lee Clark, II <gclark2@gmail.com>
2019-02-01 22:38:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-document.querySelector('#content').innerHTML('<p>Some text added with js</p>');
+// document.querySelector('#content').innerHTML('<p>Some text added with js</p>');
+
+
+// REFERENCE: https://developers.google.com/web/fundamentals/primers/service-workers/#register_a_service_worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('index.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
