4d3b82ba5c303d21be9fecdc4bf97f6fed6a46b9
G. Lee Clark, II <gclark2@gmail.com>
2019-03-05 20:32:13 -0500

    Updated spacing and added console info.

--- service-worker.js
+++ service-worker.js
@@ -4,11 +4,11 @@ let DATA_CACHE_NAME = 'data-cache-' + VERSION_NUMBER;
 // let PATH = '/testing-service-workers/';
 let PATH = '/';
 let urlsToCache = [
-    PATH + 'styles/app.css',
-    PATH + 'scripts/app.js',
+		PATH + 'styles/app.css',
+		PATH + 'scripts/app.js',
 	// PATH + 'index.html',
 	// PATH + 'page2.html',
-    // PATH + 'page3.html'
+		// PATH + 'page3.html'
 ];
 let newWorker;
 
@@ -18,16 +18,16 @@ console.info('urls', urlsToCache);
 self.addEventListener('install', (event) => {
 	console.info('Service Worker Installed.');
 
-    // perform install steps
-    event.waitUntil(
-		caches
+		// perform install steps
+		event.waitUntil(
+			caches
 			.open(CACHE_NAME)
-        	.then((cache) => {
-            	console.log('opened cache');
-            	cache.addAll(urlsToCache);
+			.then((cache) => {
+				console.log('opened cache');
+				cache.addAll(urlsToCache);
 			})
 			.then(() => self.skipWaiting())
-    );
+		);
 });
 
 // NOTE: Activate
@@ -40,16 +40,16 @@ self.addEventListener('activate', (event) => {
 
 	// NOTE: Remove old caches.
 	event.waitUntil(
-	  caches.keys().then(cacheList => {
+		caches.keys().then(cacheList => {
 		return Promise.all(cacheList.map(name => {
-		  if (cacheKeeplist.indexOf(name) === -1) {
-			  console.info(`Removing cache: ${name}`)
+			if (cacheKeeplist.indexOf(name) === -1) {
+				console.info(`Removing cache: ${name}`)
 			return caches.delete(name);
-		  }
+			}
 		}));
-	  })
+		})
 	);
-  });
+	});
 
 console.info('caches', caches);
 
@@ -62,42 +62,44 @@ self.addEventListener('message', function (event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			console.info('Cache Response', response);
+			// Cache hit - return response
+			if (response) {
+				return response;
+			}
 
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
+			return fetch(event.request).then(
+				function(response) {
+					// Check if we received a valid response
+					if(!response || response.status !== 200 || response.type !== 'basic') {
+						return response;
+					}
 
-            caches.open(CACHE_NAME)
-				.then(function(cache) {
-					console.info('Cache Info',cache);
-					cache.put(event.request, responseToCache);
-				});
+					// IMPORTANT: Clone the response. A response is a stream
+					// and because we want the browser to consume the response
+					// as well as the cache consuming the response, we need
+					// to clone it so we have two streams.
+					var responseToCache = response.clone();
 
-            return response;
-          }
-        );
-      })
-    );
+					caches.open(CACHE_NAME)
+			.then(function(cache) {
+				console.info('Cache Info',cache);
+				cache.put(event.request, responseToCache);
+			});
+
+					return response;
+				}
+			);
+		})
+	);
 });
 
 self.addEventListener('push', function(event) {
-    event.waitUntil(
-      self.registration.showNotification('Hello!', options)
+		event.waitUntil(
+			self.registration.showNotification('Hello!', options)
 	);
 });
 
@@ -105,10 +107,10 @@ self.addEventListener('push', function(event) {
 
 
 
-  /*
-  * REFERENCES (in no particular order)
-  * https://developers.google.com/web/fundamentals/primers/service-workers/#register_a_service_worker
-  * https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
-  * https://www.youtube.com/watch?v=ksXwaWHCW6k
-  *
-  */
+	/*
+	* REFERENCES (in no particular order)
+	* https://developers.google.com/web/fundamentals/primers/service-workers/#register_a_service_worker
+	* https://deanhume.com/displaying-a-new-version-available-progressive-web-app/
+	* https://www.youtube.com/watch?v=ksXwaWHCW6k
+	*
+	*/
